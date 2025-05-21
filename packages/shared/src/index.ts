fedf8e2a304b16dd74d6aad72e5806be7458626e
Muatasim-Aswad <malasoad2021@gmail.com>
2025-05-21 09:40:40 +0200

    fix a build problem

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,5 +1,5 @@
 export * as zSchema from "./zSchema/index.js";
-export * as utils from "./utils/index.js";
+export * from "./utils/index.js";
 export type * as Types from "./types/index.js";
 export type * from "./global.js";
 
