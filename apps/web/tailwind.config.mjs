b22be0ce97ad6a0483b3c5983c7e2829a2880bd4
nima <nimasaghi.dev@gmail.com>
2025-06-04 21:48:09 +0200

    fix: auto-format files using Prettier

---
+++ apps/web/tailwind.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#51A9f5",
+        secondary: "#f55a5c",
+        error: "#f43f5e",
+      },
+      fontFamily: {
+        sans: ['"Plus Jakarta Sans"', "Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
